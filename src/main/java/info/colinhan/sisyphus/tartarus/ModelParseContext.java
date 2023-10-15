107ae725ebabcd86304827ae14b4b55b94ffda38
Colin <biz@colinhan.info>
2023-10-15 11:13:19 +0800

    Support type inference for the varabile of while and paralle statement ‘

--- src/main/java/info/colinhan/sisyphus/tartarus/ModelParseContext.java
+++ src/main/java/info/colinhan/sisyphus/tartarus/ModelParseContext.java
@@ -1,6 +1,12 @@
 package info.colinhan.sisyphus.tartarus;
 
 import info.colinhan.sisyphus.context.VariableValidationContext;
+import info.colinhan.sisyphus.model.VariableType;
 
 public interface ModelParseContext extends VariableValidationContext {
+    void pushFrame();
+
+    void popFrame();
+
+    void setVariableType(String variableName, VariableType variableType);
 }
